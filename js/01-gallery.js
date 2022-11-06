f6999caa297198a3d4b2c892042539c76d1521fc
Blue-Yeti <112828764+Blue-Yeti@users.noreply.github.com>
2022-11-06 20:27:59 +0200

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(el => `
+<div class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+        <img
+        class="gallery__image"
+        src="${el.preview}" 
+        data-source="${el.original}" 
+        // data-preview="${el.preview}" 
+        alt="${el.description}"/>
+    </a>
+</div>
+`)
+
+
+galleryEl.innerHTML = markup.join('');
+
+// on click model box 
+function createLightBox(img_link) {
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${img_link}">
+    `)
+    instance.show();
+
+  }
+
+
+function handleImg(event) {
+    event.preventDefault()
+    if (event.target.nodeName === 'IMG') {
+        createLightBox(event.target.dataset.source)
+        document.addEventListener('keydown', onEscapeClose)
+    }
+    
+}
+
+function onEscapeClose(event) {    
+    if (event.code === 'Escape') {
+        console.log('В этом месте могло бы сработать instance.close()');
+        alert('В этом месте могло бы сработать instance.close()');
+        document.removeEventListener('keydown', onEscapeClose)
+        alert('EventListener has been removed!');
+
+}
+    
+}
+galleryEl.addEventListener('click', handleImg);
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(el => `
+<div class="gallery">
+    <a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+    </a>
+</div>
+`)
+
+galleryEl.innerHTML = markup.join('');
+
+let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
