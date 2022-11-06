2444e85684ba09bad52244c091437c870b483f21
Blue-Yeti <112828764+Blue-Yeti@users.noreply.github.com>
2022-11-06 20:40:37 +0200

    102

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,11 +5,11 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
 
 const markup = galleryItems.map(el => `
-<div class="gallery">
+
     <a class="gallery__item" href="${el.original}">
     <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
     </a>
-</div>
+
 `)
 
 galleryEl.innerHTML = markup.join('');
